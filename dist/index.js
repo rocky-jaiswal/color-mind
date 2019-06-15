72d30ed9916846789f152edc04764a395b975c58
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2019-06-15 16:06:59 +0200

    Add some validation.

--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const axios_1 = require("axios");
 const API_URL = 'http://colormind.io/api/';
 const generate = async (input = [], model = 'default') => {
+    if (!input || input.length > 5) {
+        throw new Error('Input array cannot be undefined, null or longer than 5 strings.');
+    }
     const response = await axios_1.default.post(API_URL, { model, input: padWith(input).map(hexToDec) });
     return response
         .data
@@ -22,7 +25,14 @@ const hexToDec = (hex) => {
     if (hex === 'N') {
         return 'N';
     }
-    return [parseInt(hex.substr(1, 2), 16), parseInt(hex.substr(3, 2), 16), parseInt(hex.substr(5, 2), 16)];
+    if (hex.length !== 7) {
+        throw new Error('Input colors have to be of format "#a1a1a1"');
+    }
+    const rgbs = [parseInt(hex.substr(1, 2), 16), parseInt(hex.substr(3, 2), 16), parseInt(hex.substr(5, 2), 16)];
+    if (rgbs.filter((rgb) => isNaN(rgb)).length > 0) {
+        throw new Error('Invalid hex color provided.');
+    }
+    return rgbs;
 };
 exports.default = generate;
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import axios from 'axios'
 const API_URL = 'http://colormind.io/api/'
 
 const generate = async (input: string[] = [], model: 'ui' | 'default' = 'default'): Promise<string[]> => {
+  if (!input || input.length > 5) {
+    throw new Error('Input array cannot be undefined, null or longer than 5 strings.')
+  }
   const response = await axios.post(API_URL, { model, input: padWith(input).map(hexToDec) })
   return response
     .data
@@ -22,7 +25,16 @@ const hexToDec = (hex: string) => {
   if (hex === 'N') {
     return 'N'
   }
-  return [parseInt(hex.substr(1, 2), 16), parseInt(hex.substr(3, 2), 16), parseInt(hex.substr(5, 2), 16)]
+  if (hex.length !== 7) {
+    throw new Error('Input colors have to be of format "#a1a1a1"')
+  }
+
+  const rgbs = [parseInt(hex.substr(1, 2), 16), parseInt(hex.substr(3, 2), 16), parseInt(hex.substr(5, 2), 16)]
+
+  if (rgbs.filter((rgb: number) => isNaN(rgb)).length > 0) {
+    throw new Error('Invalid hex color provided.')
+  }
+  return rgbs
 }
 
 export default generate
