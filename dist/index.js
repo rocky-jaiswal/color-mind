6745675cd36dd59f53ee774f3cec03ddfa139d0f
Rocky Jaiswal <rocky.jaiswal@gmail.com>
2019-06-15 13:24:41 +0200

    minor fixes.

--- dist/index.js
+++ dist/index.js
@@ -24,6 +24,5 @@ const hexToDec = (hex) => {
     }
     return [parseInt(hex.substr(1, 2), 16), parseInt(hex.substr(3, 2), 16), parseInt(hex.substr(5, 2), 16)];
 };
-generate(['#cccccc']).then(console.log);
 exports.default = generate;
 //# sourceMappingURL=index.js.map
